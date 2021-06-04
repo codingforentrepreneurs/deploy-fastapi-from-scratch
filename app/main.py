23507904fcc1884fc79d06f8b578e0022de532a4
CFE <hello@teamcfe.com>
2021-06-04 13:58:31 -0700

    Updated main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
+import pathlib
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+ROOT_PROJECT_DIR = BASE_DIR.parent
+TEMPLATE_DIR = ROOT_PROJECT_DIR / "html" # /var/www/html/
 
 app = FastAPI()
 
-@app.get("/") # html -> localhost:8000/
+templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
+
+@app.get("/", response_class=HTMLResponse) # html -> localhost:8000/
 def read_index():
-    return {"hello": "world this is supervisor"}
+    return templates.TemplateResponse("index.nginx-debian.html", {})
 
 @app.get("/abc") # html -> localhost:8000/abc
 def read_abc():
