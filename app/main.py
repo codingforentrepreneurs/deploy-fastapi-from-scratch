cacbfcd9f0d30ad8350c4f8bf9692c62e0739812
CFE <hello@teamcfe.com>
2021-08-16 17:16:49 -0500

    Added hostname for load balancer

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from ip import get_ip
+
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 ROOT_PROJECT_DIR = BASE_DIR.parent
 TEMPLATE_DIR = ROOT_PROJECT_DIR / "html" # /var/www/html/
@@ -12,7 +14,7 @@ templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
 
 @app.get("/", response_class=HTMLResponse) # html -> localhost:8000/
 def read_index(request:Request):
-    return templates.TemplateResponse("index.nginx-debian.html", {"request": request, "title": "Hello World from Jinja"})
+    return templates.TemplateResponse("index.nginx-debian.html", {"request": request, "title": "Hello World from Jinja", "hostname": get_ip() })
 
 @app.get("/abc") # html -> localhost:8000/abc
 def read_abc():
