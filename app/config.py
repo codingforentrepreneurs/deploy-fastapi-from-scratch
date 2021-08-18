608d7424d94574255ce90b8f80f0e8eb50891daf
CFE <hello@teamcfe.com>
2021-08-17 21:39:56 -0500

    Added db environment vars

---
+++ app/config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "Fastapi APP"
+    APP_DB: str = None
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
--- app/main.py
+++ app/main.py
@@ -3,14 +3,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from config import get_settings
 from ip import get_ip
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 ROOT_PROJECT_DIR = BASE_DIR.parent
 TEMPLATE_DIR = ROOT_PROJECT_DIR / "html" # /var/www/html/
 app = FastAPI()
+settings = get_settings()
 
 templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
+
 
 @app.get("/", response_class=HTMLResponse) # html -> localhost:8000/
 def read_index(request:Request):
@@ -18,4 +21,4 @@ def read_index(request:Request):
 
 @app.get("/abc") # html -> localhost:8000/abc
 def read_abc():
-    return {"hello": "abc"}
+    return {"hello": "abc", "db": settings.APP_DB is not None}
